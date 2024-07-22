a6af711ede8a72302e27562722c0d5fb5befd2d3
KamilGarbacki <kamil.garbacki06@gmail.com>
2024-07-23 01:46:20 +0200

    frontend react workflow fix 5

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         getCustomers().then(res => {
             setCustomers(res.data)
         }).catch(err => {
-
+            console.log("a");
             setError(err.response.data.message)
             errorNotification(
                 err.code,
