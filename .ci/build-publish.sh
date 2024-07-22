b8e47829a7c7e65b694702bbd569541a913ca2ff
KamilGarbacki <kamil.garbacki06@gmail.com>
2024-07-23 01:32:37 +0200

    frontend react workflow fix 2

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -2,12 +2,12 @@
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-docker buildx create -use
+docker buildx create --use
 
 docker buildx build \
-  --platform=linux/arm64, linux/amd64
-  -t "${USERNAME}/${REPO}:${TAG}"
-  -t "${USERNAME}/${REPO}:latest"
-  "${@:2}"
-  -push \
-  "$1"
+    --platform=linux/amd64,linux/arm64 \
+    -t "${USERNAME}/${REPO}:${TAG}" \
+    -t "${USERNAME}/${REPO}:latest" \
+    "${@:2}" \
+    --push \
+    "$1"
