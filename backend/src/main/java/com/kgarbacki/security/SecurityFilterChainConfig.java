738f02536e4b76e522e3dc3366dfff2d7111b6d4
KamilGarbacki <kamil.garbacki06@gmail.com>
2024-08-16 13:33:24 +0200

    pls work

--- backend/src/main/java/com/kgarbacki/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/kgarbacki/security/SecurityFilterChainConfig.java
@@ -30,7 +30,7 @@ public class SecurityFilterChainConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.authenticationEntryPoint = authenticationEntryPoint;
     }
-
+//aaa
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
